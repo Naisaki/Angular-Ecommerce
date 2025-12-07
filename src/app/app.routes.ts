1a400d53bfc6cec931c06b921cf367668049454d
ArianJC2002 <arianconqui@gmail.com>
2025-12-06 22:15:25 -0500

    feat: implement application routing with lazy-loaded components and configure TypeScript path aliases for module resolution.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,70 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Home
+  {
+    path: '',
+    loadComponent: () =>
+      import('@features/home/pages/home-page/home-page').then(
+        (m) => m.HomePage
+      ),
+  },
+
+  // Catálogo
+  {
+    path: 'catalog',
+    loadComponent: () =>
+      import(
+        '@features/catalog/pages/catalog-page/catalog-page'
+      ).then((m) => m.CatalogPage),
+  },
+  {
+    path: 'product/:id',
+    loadComponent: () =>
+      import(
+        '@features/catalog/pages/product-detail-page/product-detail-page'
+      ).then((m) => m.ProductDetailPage),
+  },
+
+  // Carrito
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('@features/cart/pages/cart-page/cart-page').then(
+        (m) => m.CartPage
+      ),
+  },
+
+  // Checkout
+  {
+    path: 'checkout',
+    loadComponent: () =>
+      import(
+        '@features/checkout/pages/checkout-page/checkout-page'
+      ).then((m) => m.CheckoutPage),
+  },
+
+  // Auth
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('@features/auth/pages/login-page/login-page').then(
+        (m) => m.LoginPage
+      ),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import(
+        '@features/auth/pages/register-page/register-page'
+      ).then((m) => m.RegisterPage),
+  },
+
+  // Fallback
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
