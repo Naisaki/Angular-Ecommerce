f158cfafbaa81bdecea27f786e63c16a2614b3fb
ArianJC2002 <arianconqui@gmail.com>
2025-12-07 10:00:49 -0500

    feat: implement navbar component with light/dark theme switching and global styling.

--- src/app/shared/layout/navbar/navbar.ts
+++ src/app/shared/layout/navbar/navbar.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,37 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
+export class Navbar implements OnInit {
+  isDarkMode = false;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // Leer tema guardado (si existe)
+      const savedTheme = localStorage.getItem('theme');
+
+      if (savedTheme === 'dark') {
+        this.isDarkMode = true;
+        document.documentElement.classList.add('dark');
+      } else {
+        this.isDarkMode = false;
+        document.documentElement.classList.remove('dark');
+      }
+    }
+  }
+
+  toggleTheme(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isDarkMode = !this.isDarkMode;
+
+      if (this.isDarkMode) {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+      }
+    }
+  }
 }
