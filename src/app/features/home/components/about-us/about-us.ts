cd8cf2508b93b1a702b5bdb6e50825d9f00f0b50
ArianJC2002 <arianconqui@gmail.com>
2026-01-09 15:47:53 -0500

    feat(about-us): update styles and integrate Button component

--- src/app/features/home/components/about-us/about-us.ts
+++ src/app/features/home/components/about-us/about-us.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Button } from '@shared/ui/button/button';
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [Button],
   templateUrl: './about-us.html',
   styleUrl: './about-us.css',
 })
