8fe21a6af6a1608972b4a8b033ba32a32e13bd6f
ArianJC2002 <arianconqui@gmail.com>
2026-01-09 15:55:31 -0500

    feat(product-grid): implement product grid component

--- src/app/features/catalog/components/product-grid/product-grid.ts
+++ src/app/features/catalog/components/product-grid/product-grid.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductCard } from '@shared/ui/product-card/product-card';
+import { Product } from '@features/catalog/models/product';
 
 @Component({
   selector: 'app-product-grid',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './product-grid.html',
   styleUrl: './product-grid.css',
 })
 export class ProductGrid {
+  @Input() products: Product[] = [];
+  isLoading = false;
 
+  clearFilters() {
+    // lógica para resetear filtros
+  }
 }
