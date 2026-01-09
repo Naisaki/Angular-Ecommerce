318f905a3f1963dc6d9d74efb2ff1635188bfccf
ArianJC2002 <arianconqui@gmail.com>
2026-01-09 15:53:06 -0500

    feat(footer): update styles and integrate Button component

--- src/app/shared/layout/footer/footer.ts
+++ src/app/shared/layout/footer/footer.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Button } from '@shared/ui/button/button';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [Button],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
